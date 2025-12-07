22fc8af9c5b7369cf33aa61f0e62c66c73b8b413
Jedsada Meesuk <6733037621@student.chula.ac.th>
2025-12-07 15:38:47 +0700

    chore: add firebase-admin dependency to package.json

---
+++ config/firebase.js
@@ -0,0 +1,16 @@
+var admin = require("firebase-admin");
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL 
+}
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DB_URL
+  });
+}
+
+const db = admin.database();
+module.exports = db;
